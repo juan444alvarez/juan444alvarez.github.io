85cae0df362ae68ddb45d13676562f6ab17710d0
juan444alvarez <97936530+juan444alvarez@users.noreply.github.com>
2022-01-25 05:53:45 -0800

    Delete responsive-portfoli0vfinal directory

--- responsive-portfoli0vfinal/app.js
+++
@@ -1,5 +0,0 @@
-const tl = gsap.timeline({defaults: {duration: 1.2, ease: "sine.out"}})
-tl.fromTo('.text', {opacity: 0}, {opacity: 1})
-tl.fromTo('.nav-header', {opacity: 0}, {opacity: 1}, '<')
-tl.fromTo('.id-card', {opacity: 0}, {opacity: 1})
-tl.fromTo('.footer', {opacity: 0}, {opacity: 1})
