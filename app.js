8b513d81a14c56b5aba3a1cad9d91003900dbc0c
juan444alvarez <97936530+juan444alvarez@users.noreply.github.com>
2022-01-29 13:25:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const tl = gsap.timeline({defaults: {duration: 1.2, ease: "sine.out"}})
+const tl = gsap.timeline({defaults: {duration: 1, ease: "power2.out"}})
 tl.fromTo('.text', {opacity: 0}, {opacity: 1})
 tl.fromTo('.nav-header', {opacity: 0}, {opacity: 1}, '<')
 tl.fromTo('.id-card', {opacity: 0}, {opacity: 1})
-tl.fromTo('.footer', {opacity: 0}, {opacity: 1})
+tl.fromTo('.footer', {opacity: 0}, {opacity: 1}, '<70%')
