b81ca628756f37519dc9b0d470bb5731887f82f6
juan444alvarez <97936530+juan444alvarez@users.noreply.github.com>
2024-06-10 20:44:55 -0700

    Update zion.js

--- zion/zion.js
+++ zion/zion.js
@@ -74,3 +74,20 @@ for (let i = 0; i < elements.length; i++) {
     }, 25);
   }
 }
+
+let progress = document.getElementById("progress");
+
+window.onscroll = function () {
+  updateProgress();
+};
+
+function updateProgress() {
+  let scrollPosition =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  let scrolled = (scrollPosition / height) * 100;
+  progress.style.width = `${scrolled}%`;
+}
